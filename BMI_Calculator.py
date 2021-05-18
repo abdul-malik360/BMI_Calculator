e949adfcfa7c1cf83c74e9730ade9d865ccb96e8
abdul-malik <abdulmalikmohamed360@gmail.com>
2021-05-18 16:32:45 +0200

    add functions

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -8,7 +8,7 @@ box = Tk()
 box.title("BMI Calculator")
 
 # give your box a size
-box.geometry("800x600")
+box.geometry("640x600")
 
 # give your box a background colour
 box.configure(bg="sky blue")
@@ -20,14 +20,22 @@ treadmill = PhotoImage(file = "treadmil.png")
 # place image
 background = Label(box, image=treadmill,).place(x=50, y=50)
 
+# create variables for entry boxes
+bmi = StringVar
+ideal = StringVar
+w_ent = StringVar
+h_ent = StringVar
+a_ent = StringVar
+
+
 # add a heading to your programme and place it
-Label(text="Ideal Box Mass Index Calculator", bg="light blue").place(x= 250, y=100)
+Label(text="Ideal Box Mass Index Calculator", bg="sky blue").place(x= 250, y=100)
 
 # give a command and place it
-Label(text="Enter your Weight, Height and Gender", bg="light blue").place(x=150, y=250)
+Label(text="Enter your Weight, Height and Gender", bg="sky blue").place(x=150, y=250)
 
 # name and put a frame down, add padding as well
-input_box = LabelFrame(box, padx=170, pady=100, bg="light blue")
+input_box = LabelFrame(box, padx=50, pady=30, bg="light blue")
 # place the frame
 input_box.place(x=50, y=300)
 
@@ -37,22 +45,65 @@ options = ["select", "Male", "Female"]
 var = StringVar(box)
 # set variable to the list options
 var.set(options[0])
-# add labels to your frame
-Label(input_box, text="Weight").grid(row=1, column=1)
-Label(input_box, text="Height").grid(row=2, column=1)
-Label(input_box, text="Gender").grid(row=3, column=1)
-Label(input_box, text="kgs").grid(row=1, column=3)
-Label(input_box, text="cm").grid(row=2, column=3)
-Label(input_box, text="Age").grid(row=3,column=3)
-# put entries in frame
-Entry(input_box).grid(row=1, column=2)
-Entry(input_box).grid(row= 2, column=2)
+# add labels to your frame, give it a background color and place them
+Label(input_box, text="Weight: ", bg="light blue").grid(row=1, column=1)
+Label(input_box, text="Height: ", bg="light blue").grid(row=2, column=1)
+Label(input_box, text="Gender: ", bg="light blue").grid(row=3, column=1)
+Label(input_box, text="kgs", bg="light blue").grid(row=1, column=3)
+Label(input_box, text="cm", bg="light blue").grid(row=2, column=3)
+Label(input_box, text="Age: ", bg="light blue").grid(row=3,column=3)
+# put entries in frame and place them, also name your entries
+weight = Entry(input_box, ).grid(row=1, column=2)
+height = Entry(input_box).grid(row=2, column=2)
+age = Entry(input_box).grid(row=3, column=4)
 # add an option menu to your frame
 opt = OptionMenu(input_box, var, *options)
-# give your menu a width
-opt.config(width=15)
+# give your menu a width and a background color
+opt.config(width=15,bg="light blue")
 # place your option menu
 opt.grid(row=3,column=2)
+
+
+# defining the function of the calculating bmi button
+def calculate():
+    input_box.weight/(height/100)**2
+    return BMI
+
+
+# create a button to calculate BMI, give it a color and place it
+Button(box, text="Calculate your Ideal BMI", bg="light blue", command=calculate).place(x=220, y=450)
+# add result labels and result boxes to your box and not the frame and place them
+# result labels
+Label(box, text="BMI: ", bg="sky blue").place(x=50, y=500)
+Label(box, text="Ideal BMI: ", bg= "sky blue").place(x=300, y=500)
+# result boxes
+BMI = Label(box, width=20).place(x=100, y=500)
+Label(box, width=20).place(x=390, y=500)
+
+
+# # defining function of the clear button
+# def clear_entries():
+#     weight.delete(0, END)
+#     height.delete(0, END)
+#     age.delete(0, END)
+
+# clear button and place it, give a color as well
+Button(box, text="CLEAR", bg="light blue", ).place(x=50, y=550)
+
+
+# defining function of the exit button
+def close_box():
+    box.destroy()
+
+
+# add exit button, give a background color add command and place it
+Button(box, text="EXIT", bg="light blue", command=close_box).place(x=390, y=550)
+
+
+
+
+
+
 
 
 
